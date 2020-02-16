eaf22e9aadcf145f190780a73bbe4514a666127f
Justin Trombley <jtrombley96@gmail.com>
2020-02-16 12:07:26 -0500

    Components Build Out

--- relief/src/App.js
+++ relief/src/App.js
@@ -1,15 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Belief</h1>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>Relief</h1>
+				<Relief />
+				<Friend />
+				<Goals />
+				<Future />
+			</header>
+		</div>
+	);
+};
 
-export default App;
+export const Relief = () => {
+	return (
+		<div>
+			Let your emotions out: <input type='text' placeholder='Let your emotions out' />
+			<button>Release</button>
+		</div>
+	);
+};
+
+export const Friend = () => {
+	return (
+		<div>
+			<button>Hear from a Friend</button>
+			<p />
+		</div>
+	);
+};
+
+export const Goals = () => {
+	return (
+		<div>
+			Set your goals: <input type='text' placeholder='Set your goals' />
+			<button>Set</button>
+		</div>
+	);
+};
+
+export const Future = () => {
+	return (
+		<div>
+			<button>See your Future</button>
+			<p />
+		</div>
+	);
+};
